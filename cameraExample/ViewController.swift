05152a5f43b4b992d64f9026bf55d37e96a9b4af
alpersalik <salika@mef.edu.tr>
2022-02-06 22:02:01 +0300

    Initial Commit

---
+++ cameraExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  cameraExample
+//
+//  Created by Alper Salık on 6.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
